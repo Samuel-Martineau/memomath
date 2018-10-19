576a1e7e20e709b27c2d42a5b02bf30ea7c378d1
Samuel Martineau <samumartineau@gmail.com>
2018-10-19 19:37:32 -0400

    Début du Jeu 1

---
+++ Assets/DestroyThisChunk.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyThisChunk : MonoBehaviour {
+
+    public float targetTimer = 30f;
+
+    private void Update()
+    {
+        targetTimer -= Time.deltaTime;
+        if(targetTimer <= 0.0f)
+        {
+            DestroyThisOldChunk();
+        }
+    }
+
+    public void DestroyThisOldChunk () {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/scripts/GenerateWorld.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateWorld : MonoBehaviour {
+
+    public Transform PosGenerate;
+    public GameObject Motif1;
+
+
+
+	// Update is called once per frame
+	void Update () 
+    {
+
+	}
+
+    public void GenerateChunk ()
+    {
+        Instantiate(Motif1, PosGenerate.position, PosGenerate.rotation);
+        gameObject.transform.Translate(22f, 0f, 0f);
+    }
+    public void DestroyAllChunks ()
+    {
+        GameObject[] chunks = GameObject.FindGameObjectsWithTag("Chunk");
+        foreach (GameObject chunk in chunks) { GameObject.DestroyImmediate(chunk); }
+        ResetTransform();
+    }
+    public void ResetTransform()
+    {
+        gameObject.transform.position = new Vector2(0f, 0f);
+    }
+}
