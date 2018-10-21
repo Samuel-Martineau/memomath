4d4b2f4f25b7cf85acd79fcce63f7d42a511e5e8
Samuel Martineau <samumartineau@gmail.com>
2018-10-20 21:13:09 -0400

    Mise à jour mineure

---
+++ Assets/LoadALevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadALevel : MonoBehaviour {
+
+    public int sceneIndex;
+
+    public LevelLoader script;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            script.LoadLevel(sceneIndex);
+        }
+    }
+}
